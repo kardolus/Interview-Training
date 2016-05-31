fc6f86b698873b95d61dc173022a8ef397500c70
kardolus <kardolus@gmail.com>
2016-05-31 16:59:41 -0700

    Added binary tree with leveled, in order, pre order, and post order traversal.

---
+++ src/us/kardol/datastructure/graph/BinaryTree.java
@@ -0,0 +1,124 @@
+package us.kardol.datastructure.graph;
+
+import java.util.LinkedList;
+
+public class BinaryTree {
+    Node root;
+    Node focus;
+    Node parent = null;
+
+    private int size = 0;
+
+    public void add(int data) {
+        if (root == null) {
+            root = new Node(data);
+            size++;
+            return;
+        }
+        focus = root;
+
+        while (focus != null) {
+            parent = focus;
+            if (data < focus.data) {
+                focus = focus.leftChild;
+            } else if (data > focus.data) {
+                focus = focus.rightChild;
+            } else {
+                return;
+            }
+        }
+
+        if (parent.data < data) {
+            parent.rightChild = new Node(data);
+        } else {
+            parent.leftChild = new Node(data);
+        }
+        size++;
+    }
+
+    public void inOrderTraverse() {
+        this.inOrderTraverse(root);
+    }
+
+    public void preOrderTraverse() {
+        this.preOrderTraverse(root);
+    }
+
+    public void postOrderTraverse() {
+        this.postOrderTraverse(root);
+    }
+
+    // depth first
+    private void inOrderTraverse(Node node) {
+        if (node.leftChild != null)
+            inOrderTraverse(node.leftChild);
+        System.out.println(node.data);
+        if (node.rightChild != null)
+            inOrderTraverse(node.rightChild);
+    }
+
+    private void preOrderTraverse(Node node) {
+        System.out.println(node.data);
+        if (node.leftChild != null)
+            preOrderTraverse(node.leftChild);
+        if (node.rightChild != null)
+            preOrderTraverse(node.rightChild);
+    }
+
+    private void postOrderTraverse(Node node) {
+        if (node.leftChild != null)
+            postOrderTraverse(node.leftChild);
+        if (node.rightChild != null)
+            postOrderTraverse(node.rightChild);
+        System.out.println(node.data);
+    }
+
+    // breadth first (for example to find your N-ordered LinkedIn connections).
+    public void levelOrderTraverse() {
+        levelOrderTraverse(root);
+    }
+
+    private void levelOrderTraverse(Node node) {
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(root);
+        Node tmpNode;
+        while (queue.size() != 0) {
+            tmpNode = queue.pop();
+            System.out.println(tmpNode.data);
+            if (tmpNode.leftChild != null) {
+                queue.add(tmpNode.leftChild);
+            }
+            if (tmpNode.rightChild != null) {
+                queue.add(tmpNode.rightChild);
+            }
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(int data) {
+        focus = root;
+        while (focus != null) {
+            if (data < focus.data) {
+                focus = focus.leftChild;
+            } else if (data > focus.data) {
+                focus = focus.rightChild;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Node {
+    int data;
+    Node leftChild;
+    Node rightChild;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
---
+++ test/us/kardol/datastructure/graph/BinaryTreeTest.java
@@ -0,0 +1,77 @@
+package us.kardol.datastructure.graph;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gkardol on 5/31/16.
+ */
+public class BinaryTreeTest {
+    BinaryTree binaryTree;
+
+    @Before
+    public void setUp(){
+        binaryTree = new BinaryTree();
+    }
+
+    @Test
+    public void testAddOne(){
+        assertEquals(binaryTree.size(),0);
+        assertFalse(binaryTree.contains(5));
+        binaryTree.add(5);
+        assertEquals(binaryTree.size(), 1);
+        assertTrue(binaryTree.contains(5));
+    }
+
+    @Test
+    public void testAddMany(){
+        addMany();
+        assertTrue(binaryTree.contains(6));
+    }
+
+    @Test
+    public void testAddDuplicate(){
+        addMany();
+        binaryTree.add(2);
+        assertEquals(binaryTree.size(), 8);
+    }
+
+    @Test
+    public void testInOrderTraverse(){
+        addMany();
+        binaryTree.inOrderTraverse();
+    }
+
+    @Test
+    public void testPreOrderTraverse(){
+        addMany();
+        binaryTree.preOrderTraverse();
+    }
+
+    @Test
+    public void testPostOrderTraverse(){
+        addMany();
+        binaryTree.postOrderTraverse();
+    }
+
+    @Test
+    public void testLevelOrderTraverse(){
+        addMany();
+        System.out.println("Level Order -----------");
+        binaryTree.levelOrderTraverse();
+        System.out.println("Level Order ----------- end");
+    }
+
+    private void addMany(){
+        binaryTree.add(4);
+        binaryTree.add(6);
+        binaryTree.add(8);
+        binaryTree.add(12);
+        binaryTree.add(45);
+        binaryTree.add(2);
+        binaryTree.add(3);
+        binaryTree.add(7);
+    }
+}
