a09550b8d7234a426ada56b4e431b9112ab876c9
kardolus <kardolus@gmail.com>
2016-06-08 15:47:49 -0700

    Added the Trie data structure.

---
+++ src/us/kardol/datastructure/graph/trie/Trie.java
@@ -0,0 +1,67 @@
+package us.kardol.datastructure.graph.trie;
+
+import java.util.*;
+
+public class Trie {
+    Node focus;
+    Node[] rootNodes = new Node[26];
+
+    public boolean isEmpty() {
+        for(Node node : rootNodes){
+            if(node != null){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void add(String word) {
+        int index = charToIndex(word.charAt(0));
+        if(rootNodes[index] == null){
+            rootNodes[index] = new Node(word.charAt(0));
+        }
+
+        focus = rootNodes[index];
+        char[] charArray = word.toCharArray();
+
+        for(int i = 1; i < charArray.length; i++){
+            Node newNode = new Node(charArray[i]);
+            index = charToIndex(charArray[i]);
+            focus.nodeArray[index] = newNode;
+            focus = focus.nodeArray[index];
+        }
+    }
+
+    public String complete(String word) {
+        int index = charToIndex(word.charAt(0));
+        focus = rootNodes[index];
+        String result = focus.toString();
+
+        for(int i = 0; i < focus.nodeArray.length; i++) {
+            if(focus.nodeArray[i] != null){
+                focus = focus.nodeArray[i];
+                i = 0;
+                result += focus.toString();
+            }
+        }
+        return result;
+    }
+
+    private int charToIndex(char c){
+        return c - 97;
+    }
+}
+
+class Node{
+    Node[] nodeArray = new Node[26];
+    char letter;
+
+    Node(char letter){
+        this.letter = letter;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(letter);
+    }
+}
---
+++ test/us/kardol/datastructure/graph/trie/TrieTest.java
@@ -0,0 +1,34 @@
+package us.kardol.datastructure.graph.trie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrieTest {
+    private Trie trie;
+
+    @Before
+    public void setUp(){
+        trie = new Trie();
+    }
+
+    @Test
+    public void testIsEmpty(){
+        assertTrue(trie.isEmpty());
+    }
+
+    @Test
+    public void testAddOne(){
+        trie.add("apple");
+        assertEquals(trie.complete("a").toString(), "apple");
+    }
+
+    @Test
+    public void testAddMany(){
+        trie.add("apple");
+        trie.add("ape");
+        assertEquals(trie.complete("ap").toString(), "ape");
+        assertEquals(trie.complete("a").toString(), "ape");
+    }
+}
