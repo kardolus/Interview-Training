9a76e9b940d6d086a3944fbc6fe429442704038e
kardolus <kardolus@gmail.com>
2016-06-10 14:44:23 -0700

    Added the Chain Of Responsibility pattern

--- src/us/kardol/designpattern/chainofresponsibility/ConcreteHandler1.java
+++ src/us/kardol/designpattern/chainofresponsibility/ConcreteHandler1.java
@@ -5,7 +5,6 @@ public class ConcreteHandler1 extends Handler {
     public String handleRequest(String message) {
         setSuccessor(new ConcreteHandler2());
 
-
         if(message != null){
             return "handler1";
         }
